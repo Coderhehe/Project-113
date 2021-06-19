d40672887a686e40fc20e49e9908e6f14285dfb0
Coderhehe <77240747+Coderhehe@users.noreply.github.com>
2021-06-19 11:19:04 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function setup() {
+    canvas=createCanvas(640,480);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+}
+
+function draw() {
+    image(video,0,0,200,200)
+    fill(100,60,190);
+    stroke(100,60,190);
+    circle(50,50,80);
+    circle(50,430,80);
+    circle(590,430,80);
+    circle(590,50,80);
+    fill(90,200,150);
+    stroke(90,200,150);
+    rect(90,40,460,20);
+    rect(90,420,460,20);
+    rect(40,90,20,300);
+    rect(580,90,20,300);
+}
